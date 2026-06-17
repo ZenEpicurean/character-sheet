f58f3bc05362da8d037c63c901bd530569819a88
ZenEpicurean <169108557+ZenEpicurean@users.noreply.github.com>
2026-06-16 21:58:48 -0500

    Update Profile base class with catalog structure and craft method

--- Models/Skills/Profiles/Profile.js
+++ Models/Skills/Profiles/Profile.js
@@ -1,9 +1,28 @@
 export default class Profile {
-    constructor() {
+    #name;
+    #catalog; // Map of itemID -> item details
 
+    constructor(name, catalog = {}) {
+        this.#name = name;
+        this.#catalog = catalog;
     }
 
-    produce(itemID) {
+    get name() {
+        return this.#name;
+    }
 
+    get catalog() {
+        return this.#catalog;
+    }
+
+    craft(itemID) {
+        if (!this.#catalog[itemID]) {
+            throw new Error(`Item "${itemID}" not found in ${this.#name} catalog.`);
+        }
+        return this.#catalog[itemID];
+    }
+
+    getAvailableItems() {
+        return Object.keys(this.#catalog);
     }
 }
